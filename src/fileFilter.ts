c91ef9656227cde501065f35bc12da5a489c3094
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-12-29 21:06:29 +0530

    feat : no longer support .pdf files

--- src/fileFilter.ts
+++ src/fileFilter.ts
@@ -4,16 +4,13 @@ import path from 'path'
 type MulterFile = Express.Multer.File
 
 const fileFilter = (_req : Request, file : MulterFile, cb : Function) => {
-    const allowedMimeTypes = ['image/png', 'image/jpeg', 'application/pdf']
-    const allowedExt = ['.png', '.jpg', '.jpeg', '.pdf']
+    const allowedMimeTypes = ['image/png', 'image/jpeg']
+    const allowedExt = ['.png', '.jpg', '.jpeg']
 
     const ext = path.extname(file.originalname).toLowerCase()
 
     if (allowedMimeTypes.includes(file.mimetype) && allowedExt.includes(ext)) {
         cb(null, true)
-    }
-    else if (file.size === 0) {
-        cb(new Error('EMPTY_FILE'), false)
     }
     else {
         cb( new Error('INVALID_FILE_TYPE'), false )
